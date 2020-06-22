13559d325b191de138338e2135d831c51bcdfe6a
katrina <katrina.nov13@gmail.com>
2020-06-22 16:06:29 +0300

    refact var to let

--- hard-13/script.js
+++ hard-13/script.js
@@ -10,10 +10,10 @@ const chooseColor = function() {
 
 // в зависимости от цвета высчитывает наиболее контрастный белый или черный
 function getContrastYIQ(hexcolor) {
-  var r = parseInt(hexcolor.substr(1,2), 16);
-  var g = parseInt(hexcolor.substr(3,2), 16);
-  var b = parseInt(hexcolor.substr(5,2), 16);
-  var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  let r = parseInt(hexcolor.substr(1,2), 16);
+  let g = parseInt(hexcolor.substr(3,2), 16);
+  let b = parseInt(hexcolor.substr(5,2), 16);
+  let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
   return (yiq >= 128) ? 'black' : 'white';
 }
 
